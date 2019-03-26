40841495c17b92c90dcf8dd6ccb76f95ac805dce
You-Ri <you-ri@sasakingdom.com>
2019-03-26 16:57:00 +0900

    テストを追加(WIP)

---
+++ Assets/VMCStudio/Tests/Runtime/VMCLipSyncControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+namespace VMCStudio.Tests
+{
+    public class VMCLipSyncControllerTest
+    {
+        class MoveUpDirection_TestScenario : VMCLipSyncController, IMonoBehaviourTest
+        {
+            public bool IsTestFinished { get; private set; }
+
+            private void Start ()
+            {
+                StartCoroutine (TestScenario ());
+            }
+
+            private IEnumerator TestScenario ()
+            {
+                var go = new GameObject ();
+                go.AddComponent<VMCLipSyncController> ();
+
+                // 1フレーム待機
+                yield return new WaitForSeconds (1);
+                yield return null;
+
+                // ここまで到達したらテストは成功
+                IsTestFinished = true;
+
+                // 他のテストを実行中にもこのMonoBehaviourが動いてしまうので止める
+                gameObject.SetActive (false);
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator ObjectMoveUpDirection ()
+        {
+            yield return new MonoBehaviourTest<MoveUpDirection_TestScenario> ();
+        }
+    }
+}
